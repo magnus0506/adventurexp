1ffb2bec28d415952216bb4e86fd871267b954c9
Magnus <47506157+magnus0506@users.noreply.github.com>
2021-03-24 17:39:08 +0100

    Update SecurityConfig.java

--- src/main/java/com/adventurexp/config/SecurityConfig.java
+++ src/main/java/com/adventurexp/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.authorizeRequests();
                 
                 http.csrf().disable();
 
